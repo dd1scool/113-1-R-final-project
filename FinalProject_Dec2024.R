15bf87549c6937a9e58291d166e5c53262ea4bac
econmtlin <derekctsao@gmail.com>
2024-12-18 23:49:38 +0000

    Purpose Outlined

--- FinalProject_Dec2024.R
+++ FinalProject_Dec2024.R
@@ -1,4 +1,9 @@
-# Final Project by Derek
-# Testing upload
-#Testing second commit
-#testing push
+# Final Project by Derek (002)
+# This project will try to conduct sentimental analysis on online forum comments.
+# The purpose of this project is for personal practice and evaluating applicability for future projects.
+# One application example is exploring relationship between Bitcoin price and Influencer activities 
+# For the above example, the key task is to accurately interpret netizens' reactions, position or negative, to the post.
+# In this project, I will import a database of a small sample of online forum comments
+# Then, I will try to parse it, and interpret it.
+# The goal is for the program to give a verdict of whether the post is Positive, Neutral or Negative.
+# The final part of the project is to give a nice visualization of the interpretation.
